9573073fe9a88e1d30459b980fa01447278454cd
Chino <victort8012@gmail.com>
2022-03-11 15:26:47 -0300

    Preparando app para el deploy

---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import style from './navbar.module.css'
+import { ActiveLink } from './ActiveLink';
+
+const menuItems = [
+  {
+    text: 'Home',
+    href: '/'
+  },
+  {
+    text: 'About',
+    href: '/about'
+  },
+  {
+    text: 'Contact',
+    href: '/contact'
+  },
+  {
+    text: 'Pricing',
+    href: '/pricing'
+  }
+]
+
+export const Navbar = () => {
+  return (
+    <nav className={style['menu-container']}>
+      {
+        menuItems.map(({href, text}) => (
+          <ActiveLink 
+            key={ href } 
+            href={ href } 
+            text={ text } 
+          />
+        ))
+      }
+    </nav>
+  )
+}
---
+++ pages/pricing/index.tsx
@@ -0,0 +1,17 @@
+import { MainLayout } from '../../components/layouts/MainLayout';
+import Link from 'next/link';
+
+export default function PricingPage() {
+  return (
+    <MainLayout>
+      <h1>Pricing page</h1>
+      <h1 className={'title'}>
+        Ir a <Link href="/">Home</Link>
+      </h1>
+      <p className={'description'}>
+        Get started by editing{' '}
+        <code className={'code'}>pages/pricing/index.js</code>
+      </p>
+    </MainLayout>
+  );
+}
